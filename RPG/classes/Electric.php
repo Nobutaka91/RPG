e158cac21135d6bca5d6329d514fd3f84d5055b9
otamasan8 <hatarakuotamasan8@outlook.jp>
2022-04-22 08:54:20 +0900

    敵クラスのギャラドス用の技を追加した

--- RPG/classes/Electric.php
+++ RPG/classes/Electric.php
@@ -12,11 +12,13 @@ class Electric extends Pokemon
         if (rand(1, 5) === 5) {
             //強力な技の発動
             echo "『" . $this->name . "』のかみなり! \n";
+            echo "効果はばつぐんだ! \n";
             echo $enemy->name . "に" . $this->attackPoint * 5 . " のダメージ! \n";
             $enemy->tookDamage($this->attackPoint * 5);
         } elseif (rand(1, 3) === 3) {
             //ノーマル技の発動
             echo "『" . $this->name . "』の電気ショック! \n";
+            echo "効果はばつぐんだ! \n";
             echo $enemy->name . "に" . $this->attackPoint * 3 . " のダメージ! \n";
             $enemy->tookDamage($this->attackPoint * 3);
         }  else {
--- RPG/classes/Enemy.php
+++ RPG/classes/Enemy.php
@@ -5,13 +5,31 @@ class Enemy
     const MAX_HITPOINT = 100;
     public $name;
     public $hitPoint = 100;
-    public $attackPoint = 20;
+    public $attackPoint = 10;
 
     public function doAttack($pokemon)
     {
-        echo "『" . $this->name . "』の水てっぽう! \n";
-        echo "【" . $pokemon->name. "】に" . $this->attackPoint . "のダメージ! \n";
-        $pokemon->tookDamage($this->attackPoint);
+        if (rand(1, 6) === 6) {
+            //強力な技の発動
+            echo "『" . $this->name . "』のはかいこうせん! \n";
+            echo $pokemon->name . "に" . $this->attackPoint * 5 . " のダメージ! \n";
+            $pokemon->tookDamage($this->attackPoint * 5);
+        } elseif (rand(1, 10) === 10) {
+            //ダメージ0技の発動
+            echo "『" . $this->name . "』のはねる! \n";
+            echo "しかし何も起こらなかった \n";
+        } elseif (rand(1, 3) === 3) {
+            //ノーマル技の発動
+            echo "『" . $this->name . "』のたつまき! \n";
+            echo $pokemon->name . "に" . $this->attackPoint * 2 . " のダメージ! \n";
+            $pokemon->tookDamage($this->attackPoint * 2);
+        } else {
+            //ノーマル技の発動
+            echo "『" . $this->name . "』の体当たり! \n";
+            echo $pokemon->name . "に" . $this->attackPoint . " のダメージ! \n";
+            $pokemon->tookDamage($this->attackPoint);
+        }
+        return true;
     }
 
     public function tookDamage($damage)
